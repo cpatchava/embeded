a3fa4ca3b44961597e80e4343ce4e3264aa7d108
root <cpatchava@gmail.com>
2015-04-08 23:51:57 -0700

    the commenting of the hw1.s file

--- mp1/README.md
+++ mp1/README.md
@@ -1,1 +1,9 @@
 #embeded
+
+
+I have done the assignment like you had asked for on the board.
+
+When i tried to follow your instructions and compile the hello world on my computer
+i started getting errors on dependency with device.h file that did not exist
+inside of the template.h which was included in mbed.h file. 
+
--- mp1/hw1.s
+++ mp1/hw1.s
@@ -10,7 +10,7 @@
 	.eabi_attribute 34, 0
 	.eabi_attribute 18, 4
 	.file	"hw1.cpp"
-	.global	p
+	.global	p      							#This is where the stack is setting up the global variable p
 	.bss
 	.align	2
 	.type	p, %object
@@ -30,15 +30,15 @@ main:
 	str	fp, [sp, #-4]!
 	add	fp, sp, #0
 .L2:
-	ldr	r3, .L3
-	ldr	r3, [r3, #0]
-	mov	r2, #1
-	str	r2, [r3, #0]
-	ldr	r3, .L3
-	ldr	r3, [r3, #0]
-	mov	r2, #0
-	str	r2, [r3, #0]
-	b	.L2
+	ldr	r3, .L3								#load into r3 the pointer which is p
+	ldr	r3, [r3, #0]						#load into r3 the value at r3+0
+	mov	r2, #1								#place the value 1 into r2
+	str	r2, [r3, #0]						#store r2, at r3 + 0
+	ldr	r3, .L3								#load into r3 the pointer which is p
+	ldr	r3, [r3, #0]						#load into r3 the value at r3 + 0
+	mov	r2, #0								place the value 0 into r2
+	str	r2, [r3, #0]						#store r2, at r3 + 0
+	b	.L2									#jump to .L2 (beginning of loop)
 .L4:
 	.align	2
 .L3:
